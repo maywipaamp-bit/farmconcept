30a731135e8c98965323d3b54ddddbe034963b5c
Amp <maywipa.amp@gmail.com>
2026-08-12 16:27:38 +0700

    ฟังก์ชันลบ

--- public/assets/js/data-service.js
+++ public/assets/js/data-service.js
@@ -26,6 +26,8 @@ window.TFC.dataService = function (entityKey, idField) {
   idField = idField || 'id';
   var LATENCY = 250;
   var base = window.TFC_API[entityKey] || null;
+  var masterDeleteViaPost = ['areas', 'targetGroups', 'programs', 'instructors', 'activityFormats']
+    .indexOf(entityKey) !== -1;
 
   /* ---------- โหมดฐานข้อมูลจริง ---------- */
 
@@ -61,6 +63,21 @@ window.TFC.dataService = function (entityKey, idField) {
     }
 
     return fetch(base + path, options).then(readJson);
+  }
+
+  /* บาง production server ปฏิเสธ HTTP DELETE ก่อนคำขอจะถึง Laravel
+     ส่งเป็น POST แบบฟอร์มพร้อม _method=DELETE ซึ่ง Laravel รองรับโดยตรง */
+  function removeCall(path) {
+    return fetch(base + path, {
+      method: 'POST',
+      headers: {
+        'X-CSRF-TOKEN': csrf(),
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      credentials: 'same-origin',
+      body: '_method=DELETE'
+    }).then(readJson);
   }
 
   /* ข้อมูลในหน่วยความจำต้องตามให้ทันด้วย เพราะหลายหน้าอ่าน window.TFC_MOCK ตรง ๆ
@@ -103,7 +120,10 @@ window.TFC.dataService = function (entityKey, idField) {
     },
 
     remove: function (id) {
-      return call('/' + encodeURIComponent(id), 'DELETE').then(function () { return true; });
+      var path = '/' + encodeURIComponent(id);
+      var request = masterDeleteViaPost ? removeCall(path) : call(path, 'DELETE');
+
+      return request.then(function () { return true; });
     }
   };
 
